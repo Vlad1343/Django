a443f7344b548e56fd4a5c6fc22403119ef7f62c
Vladyslav Shutkevych <vladshutkevych@gmail.com>
2025-08-24 11:12:11 +0300

    Register User

--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,12 @@
 
 from django.urls import path, include
 
+from . import views
+
 app_name = "users"
 urlpatterns = [
     # Add URL authentication URLs (login, logout, password management)
     path("", include("django.contrib.auth.urls")),
+    # Registration page
+    path("register/", views.register, name="register"),
 ]
